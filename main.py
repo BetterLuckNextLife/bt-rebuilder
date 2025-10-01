9775cf1eddefb96a4bd6bb46bb0abdd7bc7f241c
BetterLuckNextLife <rykovegor020@gmail.com>
2025-10-01 18:00:24 +0300

    Added progress bars

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pyshark
 from collections import defaultdict
 import sys
+from tqdm import tqdm
 
 if len(sys.argv) != 3:
-    print('Usage: ...')
+    print('Usage:\n    python main.py yourtraffic.pcap output.file')
     exit()
 
 filename = sys.argv[1]
 output_filename = sys.argv[2]
 
-def find_pieces(cap):
+def count_packets(cap):
+    print("[+] Counting packets...")
+    cnt = 0
+    for _ in tqdm(cap, desc="Counting packets", unit="pkt"):
+        cnt += 1
+    return cnt
+
+def find_pieces(cap, total_packets):
+    print("[+] Searching for data pieces...")
     pieces = defaultdict(list)  # index: [begin, data]
 
-    for pkt in cap:
+    for pkt in tqdm(cap, desc="Finding pieces", total=total_packets, unit="pkt"):
         if not hasattr(pkt, "bittorrent"):
             continue
         bt = pkt.bittorrent
@@ -28,20 +37,23 @@ def find_pieces(cap):
             continue
     return pieces
 
-
 def rebuild_file(pieces, output_filename):
     with open(output_filename, "wb") as out:
-        for index in sorted(pieces.keys()):
-            print(f"[+] Writing index {index}, {len(pieces[index])} pieces")
-            for begin, hexdata in sorted(pieces[index], key=lambda x: x[0]):
+        for index in tqdm(sorted(pieces.keys()), desc="Writing pieces", unit="index"):
+            piece_list = sorted(pieces[index], key=lambda x: x[0])
+            for begin, hexdata in tqdm(piece_list, desc=f"Writing index {index}", leave=False, unit="chunk"):
                 out.write(bytes.fromhex(hexdata))
 
-
 if __name__ == "__main__":
-    print("[+] Opening capture file...")
+    print("[+] Opening capture file...\nIt might take a while, because pyshark uses a ton of RAM.")
     cap = pyshark.FileCapture(filename, display_filter="bittorrent", keep_packets=False)
-    pieces = find_pieces(cap)
+
+    dumplen = count_packets(cap)
+    print(f"dump len is {dumplen}")
+
+    pieces = find_pieces(cap, dumplen)
     print(f"[+] Found {len(pieces)} pieces.") 
+
     rebuild_file(pieces, output_filename)
     print("[+] Rebuilt file successfully.")
 
